2af10bb26ccc0342c0f60d164c77fbccd9174571
Carl Thuringer <carlthuringer@gmail.com>
2017-09-26 13:37:14 -0500

    [feat ] Support before/after for run, env, add (#20)
    * Support before/after for run, env, add
    
    * Factor before/after method to a single method
    
    * [imprv] Add an exception for when before & after are used at the same time

--- lib/egg/dockerfile/base.rb
+++ lib/egg/dockerfile/base.rb
@@ -2,6 +2,13 @@ module Egg
   module Dockerfile
     # Provides shared behavior for dockerfile template classes.
     class Base
+      # Raise when before and after are both used at the same time.
+      class QuantumStateError < RuntimeError
+        def message
+          "Directives cannot happen both before and after a given directive"
+        end
+      end
+
       attr_reader :template
       attr_accessor :command
 
@@ -23,22 +30,38 @@ module Egg
         ERB.new(unrendered_output).result(binding)
       end
 
-      def run(command, before: [:cmd])
-        template.insert(do_before(before), [:run, command])
+      def run(command, before: [:cmd], after: nil)
+        insert_with_before_after([:run, command], before: before, after: after)
       end
 
-      def env(env_hash)
+      def env(env_hash, **before_after)
         env_string = env_hash.reduce("") do |out, (key, value)|
           out << key.to_s.upcase << '="' << value.to_s << '" '
         end
-        template << [:env, env_string]
+        insert_with_before_after([:env, env_string], before_after)
+      end
+
+      def add(localpath, containerpath, **before_after)
+        directive = [:add, localpath + " " + containerpath]
+        insert_with_before_after(directive, before_after)
       end
 
       private
 
-      def do_before(before)
-        template.index(before) ||
-          template.index { |tc| tc[0] == before[0] } ||
+      def insert_with_before_after(directive, before: nil, after: nil)
+        raise(QuantumStateError) if before && after
+        if before
+          template.insert(find_directive_in_template(before), directive)
+        elsif after
+          template.insert(find_directive_in_template(after) + 1, directive)
+        else
+          template << directive
+        end
+      end
+
+      def find_directive_in_template(directive)
+        template.index(directive) ||
+          template.index { |tc| tc[0] == directive[0] } ||
           -1
       end
 
