3fd1b00dd212506218a77edcf9cf4c6b64ff2646
Carl Thuringer <carlthuringer@gmail.com>
2017-11-17 12:01:10 -0600

    [feat ] Remove docker compose from egg config template

--- templates/egg_config.rb
+++ templates/egg_config.rb
@@ -10,16 +10,6 @@ Egg::Configuration.new do |config|
   # self.dockerfile.command = "bin/rails server -p 3000"
   # self.dockerfile.env(dotenv.env)
 
-  # Example docker_compose config:
-  # app = config.docker_compose.service "app"
-  # app.ports = ["3000:3000"]
-  # app.volumes = [".:/app"]
-
-  # db = config.docker_compose.service "db"
-  # db.image = "mysql"
-  # db.env "MYSQL_ROOT_PASSWORD", "mysqliswebscale"
-  # app.link db
-
   # after_startup do
   #   docker_exec "app", "rake db:setup db:seed"
   #   dotenv.set("foo", "bar")
